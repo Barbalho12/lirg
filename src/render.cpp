eac1ab9f4615ebe20907252188fed32dc7b28375
Raul Silveira <raul95@gmail.com>
2017-08-17 09:04:09 -0300

    Sphere error fixed

--- src/render.cpp
+++ src/render.cpp
@@ -23,174 +23,77 @@ using namespace utility;
 // }
 // #endif
 
-// bool hit_sphere(const Ray &r_, const point3 & c_, float radius_){
-//     auto oc = r_.get_origin() - c_;
-//     float a = dot(r_.get_direction(), r_.get_direction());
-//     float b = 2.0 * dot(oc, r_.get_direction());
-//     float c = dot(oc, oc) - (radius_* radius_);
-//     return (b*b - 4.0*a*c) >= 0;
-// }
-
-// bool hit_sphere(const Ray &ray, const point3 & center, float radius){
-//     point3 origin = ray.get_origin();
-//     vec3 t = -2 dot(origin - center);
-//     vec3 d = r_.get_direction();
-    
-//     auto oc = r_.get_origin() - c_;
-//     float a = dot(r_.get_direction(), r_.get_direction());
-//     float b = 2.0 * dot(oc, r_.get_direction());
-//     float c = dot(oc, oc) - (radius_* radius_);
-//     return (b*b - 4.0*a*c) >= 0;
-// }
-
 bool sphere_hit(Sphere s, Ray r){
-    // float p = dot((r.get_direction().x() - s.get_center().x()), (r.get_direction().x() - s.get_center().x())) +
-    //           dot((r.get_direction().y() - s.get_center().y()), (r.get_direction().y() - s.get_center().y())) +
-    //           dot((r.get_direction().z() - s.get_center().z()), (r.get_direction().z() - s.get_center().z())) -
-    //           (s.get_radius() * s.get_radius());
-
     float p = (r.get_direction() - s.get_center()).length() - s.get_radius();
 
-    if(p < 2.0){
+    if(p <= 0.0){
         return true;
     }else{
         return false;
     }
 }
 
-vec3 hit_sphere(Sphere s, Ray r){
-    vec3 a = dot(r.get_direction(), r.get_direction());
-    vec3 b = dot(((r.get_origin() - s.get_center())), r.get_direction());
-    vec3 c = dot(((r.get_origin() - s.get_center())), ((r.get_origin() - s.get_center()))) - (s.get_radius() * s.get_radius());
+// vec3 hit_sphere(Sphere s, Ray r){
+//     vec3 a = dot(r.get_direction(), r.get_direction());
+//     vec3 b = dot(((r.get_origin() - s.get_center())), r.get_direction());
+//     vec3 c = dot(((r.get_origin() - s.get_center())), ((r.get_origin() - s.get_center()))) - (s.get_radius() * s.get_radius());
 
-    vec3 t1 = (-b + sqrt(dot(b, b) - (dot(a,c))))/a;
-    vec3 t2 = (-b - sqrt(dot(b, b) - (dot(a,c))))/a;
+//     vec3 t1 = (-b + sqrt(dot(b, b) - (dot(a,c))))/a;
+//     vec3 t2 = (-b - sqrt(dot(b, b) - (dot(a,c))))/a;
 
-    if(t1 == t2 || t1.z() < t2.z()){
-        return t1;
-    }else{
-        return t2;
-    }
-}
+//     if(t1 == t2 || t1.z() < t2.z()){
+//         return t1;
+//     }else{
+//         return t2;
+//     }
+// }
 
 
-float euclidian_distance(point3 p, point3 pc){
-    // *cor = rgb(255,255,255);
-    float w =   pow (p.x() - pc.x(), 2) + 
-                pow (p.y() - pc.y(), 2) + 
-                pow (p.z() - pc.z(), 2);
-    float distance = sqrt(w);
-    return distance;
-}
+// float euclidian_distance(point3 p, point3 pc){
+//     // *cor = rgb(255,255,255);
+//     float w =   pow (p.x() - pc.x(), 2) + 
+//                 pow (p.y() - pc.y(), 2) + 
+//                 pow (p.z() - pc.z(), 2);
+//     float distance = sqrt(w);
+//     return distance;
+// }
 
-float euclidian_distance(point3 p, point3 pc, rgb *cor){
-    // *cor = rgb(1-fabs(p.x() - pc.x()),1-fabs(p.y() - pc.y()),1-fabs(p.z() - pc.z()));
-    *cor = rgb( 1-(fabs(p.x() - pc.x())+1)/2,
-                1-(fabs(p.y() - pc.y())+1)/2,
-                1-(fabs(p.z() - pc.z())+1)/2);
-    // *cor = rgb(255,255,255);
-    float w =   pow (p.x() - pc.x(), 2) + 
-                pow (p.y() - pc.y(), 2) + 
-                pow (p.z() - pc.z(), 2);
-    float distance = sqrt(w);
-    return distance;
-}
+// float euclidian_distance(point3 p, point3 pc, rgb *cor){
+//     // *cor = rgb(1-fabs(p.x() - pc.x()),1-fabs(p.y() - pc.y()),1-fabs(p.z() - pc.z()));
+//     *cor = rgb( 1-(fabs(p.x() - pc.x())+1)/2,
+//                 1-(fabs(p.y() - pc.y())+1)/2,
+//                 1-(fabs(p.z() - pc.z())+1)/2);
+//     // *cor = rgb(255,255,255);
+//     float w =   pow (p.x() - pc.x(), 2) + 
+//                 pow (p.y() - pc.y(), 2) + 
+//                 pow (p.z() - pc.z(), 2);
+//     float distance = sqrt(w);
+//     return distance;
+// }
 
-point3 p_center = point3(-1,-0.5,-1);
-
-rgb color2(Ray r_){
-    // rgb top_left (0,1,0);
-    // rgb bottom_left(0,0,0);
-    // rgb top_right (1,1,0);
-    // rgb bottom_right (1,0,0);
+rgb color(const Ray &r_){
 
     rgb top_left (0.5,0.7,1);
     rgb bottom_left(1,1,1);
     rgb top_right (0.5,0.7,1);
     rgb bottom_right (1,1,1);
 
-    point3 sphere_center = point3(0, 0, 1);
+    point3 sphere_center = point3(0, 0, -1);
     Sphere s = Sphere(sphere_center, 0.4);
 
-    
-    // if(hit_sphere(r_, point3(0.5,0,-1), 0.5)){
-    //     float h = euclidian_distance(r_.get_direction(), point3(0.5,0,-1));
-    //     return rgb(1-h,1-h,1-h);
-    // }
-
-    // auto unit_ray = utility::unit_vector(r_.get_direction());
+    //auto unit_ray = utility::unit_vector(r_.get_direction());
     auto unit_ray = r_.get_direction();
 
     rgb cor;
 
     if(sphere_hit(s, r_)){
-        vec3 v = hit_sphere(s, r_);
-        //cor = rgb(1, 0, 0);
-        cor = rgb( 1-v.x(),
-                   1-v.y(),
-                   1-v.z());
+        vec3 v = r_.get_direction() - sphere_center;
+        //vec3 v = hit_sphere(s, r_);// - sphere_center;
+
+        cor = rgb( (v.x()+1)/2.0,
+                   (v.y()+1)/2.0,
+                   (v.z()+1)/2.0);
         return cor;
-    }
-
-    float t = 0.5 * unit_ray.y() + 0.5;
-    float u = 0.25 * unit_ray.x() + 0.5;
-
-
-    // rgb result = bottom_left*(1-ty) + top_left*ty;
-    rgb result = bottom_left*(1-t)*(1-u) + 
-                 top_left*t*(1-u) + 
-                 bottom_right*u*(1-t) + 
-                 top_right*t*u;
-    // rgb result = (1-tx)*(bottom_left*(1-ty) + top_left*ty) + tx*(bottom_right*(1-ty) + top_right*ty);
-
-    // TODO: determine the background color, which is an linear interpolation between bottom->top.
-    // The interpolation is based on where the ray hits the background.
-    // Imagine that the background is attached to the view-plane; in other words,
-    // the virtual world we want to visualize is empty!
-
-    return result; // Stub, replace it accordingly
-}
-
-rgb color(const Ray & r_){
-
-    // rgb top_left (0,1,0);
-    // rgb bottom_left(0,0,0);
-    // rgb top_right (1,1,0);
-    // rgb bottom_right (1,0,0);
-
-    rgb top_left (0.5,0.7,1);
-    rgb bottom_left(1,1,1);
-    rgb top_right (0.5,0.7,1);
-    rgb bottom_right (1,1,1);
-
-    
-    // if(hit_sphere(r_, point3(0.5,0,-1), 0.5)){
-    //     float h = euclidian_distance(r_.get_direction(), point3(0.5,0,-1));
-    //     return rgb(1-h,1-h,1-h);
-    // }
-
-    // auto unit_ray = utility::unit_vector(r_.get_direction());
-    auto unit_ray = r_.get_direction();
-
-
-    point3 p1 = point3(unit_ray.x(), unit_ray.y(), unit_ray.z());
-    rgb cor;
-    float n = euclidian_distance(p1, p_center, &cor);
-    if(n <= 0.4){
-        // float h = euclidian_distance(r_.get_direction(), p_center);
-        // return rgb(1-h,1-h,1-h);
-        return cor;
-        // return rgb(p1.r()-p_center.r(),p1.g()-p_center.g(),p1.b()-p_center.b());
-    }
-
-    point3 p2 = point3(unit_ray.x()-0.6, unit_ray.y()-0.4, unit_ray.z()+0.2);
-    rgb cor2;
-    float n2 = euclidian_distance(p2, p_center, &cor2);
-    if(n2 <= 0.4){
-        // float h = euclidian_distance(r_.get_direction(), p_center);
-        // return rgb(1-h,1-h,1-h);
-        return cor;
-        // return rgb(p1.r()-p_center.r(),p1.g()-p_center.g(),p1.b()-p_center.b());
     }
 
     float t = 0.5 * unit_ray.y() + 0.5;
@@ -251,7 +154,7 @@ int makeImage(){
             Ray r(origin, end_point - origin);
 
             // Determine the color of the ray, as it travels through the virtual space.
-            rgb c = color2(r);
+            rgb c = color(r);
 
             int ir = int(255.99f * c.r());
             int ig = int(255.99f * c.g());
